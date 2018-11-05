081be7c87ac64dfcb46b256e41b9d841aa8595a4
ilcomarian <ilcomarian@gmail.com>
2018-11-05 17:05:57 -0500

    form and email

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
+import "./App.css";
+import { Navbar, Nav, NavItem, MenuItem, NavDropdown } from "react-bootstrap";
+import Routes from "./components/routs";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Navbar inverse collapseOnSelect className="heder-color">
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="/">
+                <h4 style={{ color: "black" }}> MyPortfolio</h4>
+              </a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <NavItem eventKey={1} href="/resume">
+                <h4 style={{ color: "black" }}>Resume</h4>
+              </NavItem>
+              <NavItem eventKey={2} href="/projects">
+                <h4 style={{ color: "black" }}> Projects</h4>
+              </NavItem>
+              <NavItem eventKey={1} href="/contacts">
+                <h4 style={{ color: "black" }}>Contacts</h4>
+              </NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Routes />
       </div>
     );
   }
