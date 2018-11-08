74a0862ac88e05d5b1242333f4bfef273f915873
ilcomarian <ilcomarian@gmail.com>
2018-11-07 21:39:29 -0500

    delete Link from applicationCache.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./App.css";
 import { Navbar, Nav, NavItem } from "react-bootstrap";
 import Routes from "./components/routs";
